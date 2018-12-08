72e2aaa30853f99481b6a1fcf15a27a5de440bff
Zhi Yin <Z.Yin.MD@gmail.com>
2018-12-08 18:34:34 -0500

    support color alias

--- palettes/sepia/palette.js
+++ palettes/sepia/palette.js
@@ -1,24 +1,35 @@
+// this file will be used both by node and by browser
+
 var palette = {
-	default: '#836e69',
-	grey: '#999c',
-	red: '#f00',
-	maroon: '#c10c',
-	green: '#094e',
-	blue: '#08bd',
-	yellow: '#b82',
-	purple: '#940094ab',
-	cyan: '#30a195',
-	orange: '#f77b00e6',
-	olive: '#890',
+	grey: '#836e69',
+	silver: '#999c',
+	black: '#2d2d2d',
 	pink: '#f08c',
-	teal: '#088',
-	tan: '#97733dd0',
+	purple: '#940094ab',
 	magenta: '#b36d',
-	black: '#2d2d2d'
+	maroon: '#c10c',
+	red: '#f00',
+	orange: '#f77b00e6',
+	yellow: '#b82',
+	tan: '#97733dd0',
+	olive: '#890',
+	green: '#094e',
+	cyan: '#30a195',
+	blue: '#08bd',
+	teal: '#088',
 };
 
+var alias = {
+	default: palette.grey,
+	variable: palette.olive,
+	function: palette.green,
+	parameter: palette.yellow,
+	module: palette.maroon,
+	declaration: palette.blue,
+}
+
 try {
-	module.exports = palette;
+	module.exports = Object.assign(palette, alias);
 } catch (err) {
 	// the exports can be used in node, browser doesn't understand it, but browser can use the palette.
 }
