2ca71715585a794c1168c9a7e1b1050239f08cf5
Zhi Yin <Z.Yin.MD@gmail.com>
2018-12-11 14:19:57 -0500

    improve palette, add colors

--- palettes/sepia/palette.js
+++ palettes/sepia/palette.js
@@ -1,37 +1,35 @@
 /*
-灵感: check firebreak, salmon, crimson, chocolate, Darkorchid from web colors
 目前问题:
-需要一个parameter的颜色, 考虑接近chocolate或者firebreak
-出来可以考虑名叫maroon, 然后本来的maroon叫red,  red删掉不用了, error用pink
-另外出来之后将会很接近magenta, magenta考虑不要了
 unsure现在alias到了magenta, 考虑改成一个深绿色
-
-purple不够紫, 应该再接近darkorchid一点, 反正出现频率很低
-tan和yellow合并成brown, 给string用
+如果找到合适的深绿色, 把extended class也改成深绿色, 因为现在和xxx extend xxx的extend是teal, 颜色相邻。另外extended class和class一个绿一个深绿也很不错
+make lilac color for <?php?>, refer to google image pho
 
 
  */
 // this file will be used both by node and by browser
 
 const palette = {
-	grey: '#836e69',
 	silver: '#999c',
+	neutral: '#8b6d3ccc',
+	grey: '#8d796b',
 	black: '#2d2d2d',
 	pink: '#f08c',
-	purple: '#940094ab',
+	purple: '#909a',
 	magenta: '#b36d',
 	maroon: '#c10c',
 	red: '#f00',
-	orange: '#f77b00e6',
-	yellow: '#b82',
-	tan: '#97733dd0',
+	russet: '#c51d',
+	rosy: '#e53d',
+	orange: '#f77b00e7',
+	yellow: '#b79533',
+	tan: '#96733ccc',
 	olive: '#890',
 	green: '#094e',
 	cyan: '#30a195',
 	blue: '#08bd',
+	indigo: '#78b',
 	teal: '#088',
 };
-
 
 const aliases = {
 	default: palette.grey,
@@ -39,12 +37,12 @@ const aliases = {
 	property: palette.olive,
 	function: palette.green,
 	call: palette.cyan,
-	parameter: palette.yellow,
-	argument: palette.yellow,
+	parameter: palette.indigo,
+	argument: palette.olive,
 	module: palette.maroon,
 	declaration: palette.blue,
 	tag: palette.blue,
-	string: palette.tan,
+	string: palette.yellow,
 	reserved: palette.teal,
 	builtIn: palette.teal,
 	inherited: palette.teal,
@@ -54,9 +52,9 @@ const aliases = {
 	control: palette.maroon,
 	flow: palette.black,
 	constant: palette.purple,
-	regex: palette.yellow,
+	regex: palette.rosy,
 	keyword: palette.blue,
-	unsure: palette.magenta,
+	curveball: palette.indigo,
 	invalid: palette.red,
 	deprecated: palette.pink,
 }
