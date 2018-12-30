402186ddf25c79d286104c80826d31f281d364d7
Zhi Yin <Z.Yin.MD@gmail.com>
2018-12-30 00:13:33 -0500

    refactor visualize
    it used to require the name of the theme as the argument, but later I found that building all themes together is usually better, so I just build them together

--- scripts/visualize.js
+++ scripts/visualize.js
@@ -1,43 +1,36 @@
 const fs = require('fs');
 const json = require('comment-json'); // this module supports json with comments
-const themeName = process.argv[2];
-const officialThemeName = {
-  sepia: 'Sepia',
-  cuttlefish: 'Sepia from Cuttlefish'
-}[themeName];
 
-if (!themeName || !officialThemeName) {
-  console.error(`Please provide the name of the theme you're tweaking, options are:
-  For the theme "Sepia":
-    "npm run visualize sepia"
-  For the theme "Sepia from Cuttlefish":
-    "npm run visualize cuttlefish"`);
-  process.exit();
-}
-
-const pathToUserSettings = require('./path-to-user-settings.js'); // vscode's user settings json file
-
-var userSettings;
 try {
-  userSettings = json.parse(fs.readFileSync(pathToUserSettings));
+  var pathToUserSettings = require('./path-to-user-settings.js'); // vscode's user settings json file
+  var userSettings = json.parse(fs.readFileSync(pathToUserSettings));
 } catch (err) {
   console.error("Failed to load the settings.json of VSCode, check it for trailing commas or syntax errors (comments are allowed), if you're sure settings.json is valid json, then check scripts/path-to-user-settings.js");
   throw(err);
 }
 
-const rules = require('./buildRules.js')();
-const palette = require('../palettes/_utils/lookup-css.js')(themeName);
+const themes = { // an object literal with keys being the filenames of the themes, and value being the official names of the themes in the extention
+  sepia: 'Sepia',
+  cuttlefish: 'Sepia from Cuttlefish',
+}
+const lookupCss = require('../palettes/_utils/lookup-css.js');
 
-for (let i of rules) {
-  if (i.settings.foreground) // sometimes there's no foreground property, only italic or bold
-    i.settings.foreground = palette[i.settings.foreground];
+for (let themeName in themes) {
+  const palette = lookupCss(themeName);
+  const officialThemeName = themes[themeName];
+  const rules = require('./buildRules.js')();
+  for (let i of rules) {
+    if (i.settings.foreground) // sometimes there's no foreground property, only italic or bold
+      i.settings.foreground = palette[i.settings.foreground];
+  }
+
+  // create keys in userSettings if not existed:
+  userSettings['editor.tokenColorCustomizations'] = userSettings['editor.tokenColorCustomizations'] || {};
+  userSettings['editor.tokenColorCustomizations'][`[${officialThemeName}]`] = userSettings['editor.tokenColorCustomizations'][`[${officialThemeName}]`] || {};
+
+  userSettings['editor.tokenColorCustomizations'][`[${officialThemeName}]`].textMateRules = rules;
 }
 
-// create keys in userSettings if not existed:
-userSettings['editor.tokenColorCustomizations'] = userSettings['editor.tokenColorCustomizations'] || {};
-userSettings['editor.tokenColorCustomizations'][`[${officialThemeName}]`] = userSettings['editor.tokenColorCustomizations'][`[${officialThemeName}]`] || {};
+fs.writeFileSync(pathToUserSettings, json.stringify(userSettings, null, 2));
 
-userSettings['editor.tokenColorCustomizations'][`[${officialThemeName}]`].textMateRules = rules;
-fs.writeFileSync(pathToUserSettings, json.stringify(userSettings, null, 4));
-
-console.log('Success! The changes has been effective in your VSCode.');
+console.log('Success! The changes have been effective in your VSCode.');
